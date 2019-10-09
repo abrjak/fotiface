b609257a3947d3365ac7e12b50d65d3cc77650b4
Abraham, Jakob <jakob.abraham@baloise.ch>
2019-10-09 11:16:09 +0200

    implemented new agularjs functions

---
+++ app/app.js
@@ -0,0 +1,28 @@
+var app = angular.module('fotiface', ['ngRoute']);
+app.config(function($routeProvider){
+    $routeProvider.
+    when('/home', {
+        templateUrl: 'app/views/home.template.html',
+        controller: 'HomeController'
+    }).
+    when('/user', {
+        templateUrl: 'app/views/user.template.html',
+        controller: 'UserController'
+    }).
+    otherwise({
+        redirectTo: '/home'
+    });
+});
+
+app.controller('HomeController', function($scope){
+    $scope.title = "Home Page";
+});
+
+app.controller('UserController', function($scope, $http){
+    $scope.title = "User Page";
+
+    $http.get('app/api/dbGetAllUser.php').then(function(response){
+        $scope.user = response.data;
+    })
+});
+
