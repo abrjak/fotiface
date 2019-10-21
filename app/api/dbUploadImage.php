24ba21d7897d0eec023a7b464f68c829f00453be
Jakob Abraham <jakob.abraham@gibmit.ch>
2019-10-21 18:59:54 +0200

    added mutli file upload

--- app/api/dbUploadImage.php
+++ app/api/dbUploadImage.php
@@ -1,18 +1,46 @@
 <?php
 
+session_start();
+
 include('dbConnect.php');
 
-$uploadImage = $_FILES["uploadImage"]["name"];
+$userId = $_SESSION["userId"]; 
 
 $folder = "images/";
 
-$imagePath = "$folder".$_FILES["uploadImage"]["name"];
+$total = count($_FILES["uploadImage"]["name"]);
 
-move_uploaded_file($_FILES["uploadImage"]["tmp_name"], $imagePath);
+$query = "INSERT INTO tbl_image (fk_user_id, image_path, image_name) VALUES";
 
-$query = "INSERT INTO tbl_image (fk_user_id, image_path, image_name) VALUES(1, '$imagePath', '$uploadImage')";
+for($i=0 ; $i < $total ; $i++){
+
+    $uploadImage = $_FILES["uploadImage"]["name"][$i];
+
+    $imagePath = "$folder".$_FILES["uploadImage"]["name"][$i];
+
+    move_uploaded_file($_FILES["uploadImage"]["tmp_name"][$i], $imagePath);
+
+    if($i == $total-1){
+        $query .= "('$userId', '$imagePath', '$uploadImage')";
+    } else {
+        $query .= "('$userId', '$imagePath', '$uploadImage'),";
+    }
+   
+}
+
+echo $query;
 
 $statement = $mysqli->prepare($query);
 
 $statement->execute();
+
+$statement->close();
+
+
+
+
+
+
+
+
 
--- app/views/gallery.template.php
+++ app/views/gallery.template.php
@@ -24,7 +24,7 @@
                 <div ng-repeat="image in images" class="col-lg-3 col-md-4 col-6">
                     <figure>
                     <a href="app/api/{{image.image_path}}" class="d-block mb-4 h-100">
-                        <img class="img-fluid img-thumbnail" src="app/api/{{image.image_path}}" width="400" height="300" alt="">
+                        <img class="img-fluid img-thumbnail" src="app/api/{{image.image_path}}" alt="">
                     </a>
                     </figure>
                 </div>
@@ -34,7 +34,7 @@
 <hr />
 
         <form method="POST" action="../fotiface/app/api/dbUploadImage.php" enctype="multipart/form-data">
-            <input type="file" name="uploadImage">
+            <input type="file" name="uploadImage[]" multiple>
             <input type="submit" name="submit_image" value="Upload">
         </form>
     </div>
